3b58fa127ee10448685fdebf9cf9256b6cd8c220
Oğuz Direnç DİNÇ <oguzdirenc@hotmail.com>
2023-10-15 17:23:44 +0300

    Bug fixed

--- src/main/java/com/oguzdirenc/config/ConfigApplication.java
+++ src/main/java/com/oguzdirenc/config/ConfigApplication.java
@@ -1,13 +1,23 @@
 package com.oguzdirenc.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
 	}
 
 }
