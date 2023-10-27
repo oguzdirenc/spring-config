114ce025dfb6652476a93c7bda17f38a19ab88d5
Oğuz Direnç DİNÇ <oguzdirenc@hotmail.com>
2023-10-27 14:06:46 +0300

    jasypt password encryption implemented

--- src/main/java/com/oguzdirenc/config/ConfigApplication.java
+++ src/main/java/com/oguzdirenc/config/ConfigApplication.java
@@ -1,5 +1,6 @@
 package com.oguzdirenc.config;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEncryptableProperties
 public class ConfigApplication {
 
 	public static void main(String[] args) {
